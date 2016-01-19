4d06833778637704687f76e30bc6d49788c1ea51
matthewepler <matthewepler@gmail.com>
2016-01-18 21:41:05 -0500

    through step 9

--- simple-todos/simple-todos.js
+++ simple-todos/simple-todos.js
@@ -4,7 +4,19 @@ if (Meteor.isClient) {
   // This code only runs on the client
   Template.body.helpers({
     tasks: function() {
-    	return Tasks.find({}, {sort: {createdAt: -1}});
+    	if (Session.get("hideCompleted")) {
+        // If hide completed is checked, filter tasks
+        return Tasks.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
+      } else {
+        // Otherwise, return all of the tasks
+        return Tasks.find({}, {sort: {createdAt: -1}});
+      }
+    },
+    hideCompleted: function () {
+      return Session.get("hideCompleted");
+    },
+    incompleteCount: function () {
+      return Tasks.find({checked: {$ne: true}}).count();
     }
   });
 
@@ -14,13 +26,35 @@ if (Meteor.isClient) {
   		event.preventDefault();
   		var text = event.target.text.value;
 
+      // Insert a task into the collection
   		Tasks.insert({
   			text: text,
-  			createdAt: new Date()
+        createdAt: new Date(),            // current time
+        owner: Meteor.userId(),           // _id of logged in user
+        username: Meteor.user().username  // username of logged in user
   		});
 
   		event.target.text.value = "";
-  	}
+  	},
+    "change .hide-completed input": function (event) {
+      Session.set("hideCompleted", event.target.checked);
+    }
+  });
+
+  Template.task.events({
+    "click .toggle-checked": function () {
+      // Set the checked property to the opposite of its current value
+      Tasks.update(this._id, {
+        $set: {checked: ! this.checked}
+      });
+    },
+    "click .delete": function () {
+      Tasks.remove(this._id);
+    }
+  });
+
+  Accounts.ui.config({
+    passwordSignupFields: "USERNAME_ONLY"
   });
 }
 
